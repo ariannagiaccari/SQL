d478a8f6cc3c36023cd47cae80030d5da99141d2
ariannagiaccari <ariggiaccari@gmail.com>
2024-04-04 12:30:10 +0200

    table

---
+++ table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Books (
+book_id INTEGER PRIMARY KEY,
+title TEXT VARCHAR(50) NOT NULL,
+author TEXT VARCHAR(50) NOT NULL,
+genre TEXT VARCHAR(40) NOT NULL,
+price DECIMAL(9,3),
+rating NUMERIC,
+published_year DATE,
+stock_count TEXT NOT NULL
+)

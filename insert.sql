701d7f4a21c24a967fcb8bba3f565152dbf8a814
ariannagiaccari <150803603+ariannagiaccari@users.noreply.github.com>
2024-04-07 20:06:17 +0200

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO Books VALUES (
+   1, 'A Journey to the Center', 'Jules Verne', 'Adventure', 12.9, 4.5, 1864, 'Verne Publishing' 
+);
+INSERT INTO Books VALUES(
+   2, 'War and Peace', 'Leo Tolstoy', 'Historical', 14.9, 4.8, 1869, 'Tolstoy Prints'
+);
+INSERT INTO Books VALUES(
+    3, 'Whispers of the Wind', 'Amelia Blackburn', 'Romance', 9.9, 4.2, 1982, 'Blackburn House'
+);
+INSERT INTO Books VALUES(
+    4, 'The Galactic Odyssey', 'Orion Starfield', 'Science Fiction', 19.9, 4.9,  2005, 'Nebula Press'
+)
